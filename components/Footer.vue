4ecbb5a4dc7c17d14c9cb6ed75cce4b1eef3fc02
Thasreef Mogral <35225985+ismailthasreef@users.noreply.github.com>
2023-10-19 16:09:24 +0400

    Loading,MobileMenu,Footer

---
+++ components/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+    <div id="FooterMain" class="w-full bg-[#E8E8E8] mt-10 mb-2">
+        sds
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
