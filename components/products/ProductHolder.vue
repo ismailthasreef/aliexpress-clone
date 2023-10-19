6c2975f74af9af0cccf6455e2f96068b1c23b16b
Ismail Thasreef <35225985+ismailthasreef@users.noreply.github.com>
2023-10-19 22:47:10 +0400

    taiwind config,products update

--- components/products/ProductHolder.vue
+++ components/products/ProductHolder.vue
@@ -1,8 +1,38 @@
 <template>
     <div id="`ProductHolder${product.id}`"
-        class="bg-white inline-block rounded hover:shadow-[0,0,10px,3px] rgba(0,0,0,0,0.5) cursor-pointer">
+        class="bg-white inline-block rounded hover:shadow-[0,0,10px,3px] rgba(0,0,0,0,0.5) cursor-pointer ">
         <NuxtLink :to="`/item/${product.id}`">
             <img class="rounded-t" :src="product.imageUrl" :alt="product.name">
+            <div id="ProductDetails" class="mb-2">
+                <span class="flex  justify-between gap-3 px-1 pt-1">
+                    <span class="text-[#FF6674] font-semibold ">${{ priceComputed }}</span>
+                    <span class="text-gray-500 text-sm-text line-through">${{ oldPriceComputed }}</span>
+                </span>
+
+                <span class="relative px-1 -top-1.5 text-[#FF6674] text-xs font-semibold">
+                    Extra 15% OFF
+                </span>
+
+                <div class="flex items-center gap-1 px-1 relative -top-1">
+                    <span class="bg-[#fd374f] text-white text-[9px] font-semibold rounded-sm">
+                        Welcome Deal
+                    </span>
+                    <span class="bg-[#f5f5f5] border text-[#c08562] text-[9px] font-semibold rounded-sm">Top Selling</span>
+                </div>
+                <p class="flex items-center px-1 pt-0.5 text-xs text-[#8b6d36]">
+                    5,000+ sold
+                    <Icon name="material-symbols:star-rate" color="#757575" class="ml-1.5" /> 4.7
+                </p>
+
+                <p class="px-1 flex items-start pt-0.5 text-xs text-start mb-2 text-[#252525]">
+                    {{ product.name.substring(0,60) }}
+                </p>
+
+                <p class="px-1 pb-1 flex items-start">
+                    <span class="text-[#009a66] text-xs font-semibold">Free Shipping</span>
+                </p>
+
+            </div>
         </NuxtLink>
 
     </div>
@@ -18,11 +48,12 @@ const props = defineProps({
 })
 
 const priceComputed = computed(() => {
-    return props.product.price / 100
+    let res = props.product.price
+    return res.toFixed(2)
 })
 
 const oldPriceComputed = computed(() => {
-    let res = (product.price + (product.value.price / 20)) / 100
+    let res = (props.product.price + (props.product.price))
     return res.toFixed(2)
 })
 
